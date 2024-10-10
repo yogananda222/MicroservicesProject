bdf311a7db38e0fc6e4e401423fccda2eae76512
yogananda222 <yoganandanaidu14382@gmail.com>
2024-10-10 10:14:51 +0530

    Eureka

---
+++ eureka/src/test/java/com/revshop/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revshop.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
